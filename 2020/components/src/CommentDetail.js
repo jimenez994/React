3135b862d83ce75102b14c74dfa893cfa341dccf
JJimenez <j.jimenez.994@hotmail.com>
2020-10-17 23:58:43 -0400

    _

--- 2020/components/src/CommentDetail.js
+++ 2020/components/src/CommentDetail.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import faker from 'faker';
+// import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+  var comment = props.comment;
   return (
     <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
+          <img alt="avatar" src={comment.avatar}/>
         </a>
         <div className="content">
           <a href="/" className="author">
-            {faker.name.findName()}
+            {comment.userName}
           </a>
           <div className="metadata">
             <span>
               {/* Today at 6:00pm */}
-              {faker.date.recent().toLocaleDateString()}
+              {comment.time}
             </span>
           </div>
-        <div className="text"> {faker.lorem.sentence()}</div>
+        <div className="text"> {comment.text}</div>
         </div>
       </div>
   )
--- 2020/components/src/index.js
+++ 2020/components/src/index.js
@@ -1,19 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import faker from 'faker';
+
 const App = () => {
   var loop = () => {
     var comments = [];
     for (let i = 0; i < 16; i++) {
-      comments.push(<CommentDetail key={i} />);
+      var comment = {
+        userName: faker.name.findName(),
+        avatar: faker.image.avatar(),
+        text: faker.lorem.sentence(),
+        time: faker.date.recent().toLocaleDateString()
+      }
+      comments.push(
+        <ApprovalCard  key={i}>
+          <CommentDetail comment={comment} />
+        </ApprovalCard>
+      );
     }
     return comments;
   };
   return (
     <div className="ui container comments">
-      <div className="ui three column divided grid">
+      {/* <ApprovalCard/> */}
         {loop()}
-      </div>
     </div>
   );
 };
