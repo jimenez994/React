df5fcaf3e3e1a2828bb7a4bfb08d2589f7499483
JJimenez <j.jimenez.994@hotmail.com>
2020-11-14 23:13:28 -0500

    _

--- 2020/pics/src/Search.js
+++ 2020/pics/src/Search.js
@@ -10,7 +10,7 @@ class Search extends React.Component {
       accessKey: "-0FcxxDr4Ajfzcd4bQNGl7Th7wUuyDSatDXK4SVIhmM",
     });
     unsplash.search
-      .photos("dogs", 1, 5)
+      .photos("supercar", 1, 15)
       .then(toJson)
       .then((data) => {
         this.setState({ images: data.results });
@@ -19,11 +19,12 @@ class Search extends React.Component {
 
   render() {
     console.log(this.state.images);
-    // var content = <div>Loading ... </div>
-    // if (this.state.images) {
-    let content = this.state.images.map((img) => (<img src={img.urls.small} key={img.id} alt="df" />));
-    // }
-    return <ul>{content}</ul>;
+    var content = <div>Loading ... </div>
+    if (this.state.images) {
+      content = this.state.images.map((img) => (<img src={img.urls.small} key={img.id} alt="df" />));
+    }
+    return <div>
+      {content}</div>;
   }
 }
 
