1c2460a8cdbba2da9b2887822a052170e9b7f20b
JJimenez <j.jimenez.994@hotmail.com>
2021-05-28 11:53:49 -0400

    _

---
+++ 2020/widgets/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Accordion from './components/Accordion'
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  },
+]
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return (
+    <Accordion items={ items }/>
+  )
+}
