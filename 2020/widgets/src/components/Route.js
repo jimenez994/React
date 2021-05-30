b3bc0b7102500def71dbb98f7d1a006b0e718c96
JJimenez <j.jimenez.994@hotmail.com>
2021-05-30 14:26:29 -0400

    _

--- 2020/widgets/src/components/Route.js
+++ 2020/widgets/src/components/Route.js
@@ -1,7 +1,24 @@
-
+import { useState, useEffect } from 'react';
 
 const Route = ({ path, children }) => {
-  if (window.location.pathname === path) {
+
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    }
+
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    }
+  }, [])
+
+
+
+  if (currentPath === path) {
       return children;
   }
   return null;
