9447f85be6889d9897184efea02276f953d00ce8
JJimenez <j.jimenez.994@hotmail.com>
2021-08-25 22:41:40 -0400

    _

--- 2020/weather/src/App.jsx
+++ 2020/weather/src/App.jsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import useWeather from './hooks/useWeather'
 import './main.css';
-import api from './api';
+import api from './api/weather';
 
 
 const App = () => {
-  const [weatherData, setWeatherData] = useState(null)
+  const [ currentForecast, setCurrentForecast ] = useState(null)
+  const [ forecast, search ] = useWeather("cochabamba");
 
-  const search = async () => {
-    console.log(import.meta.env);
-    const response = await api.get("/forecast.json", {
-      params: {
-        q: "paris",
-        days: 3
-      }
-    })
-    setWeatherData(response.data)
-    console.log(response.data);
-  }
+  useEffect(() => {
+    setCurrentForecast(forecast[ 0 ])
+  }, [forecast]);
+
+
   let content = <div>...</div>
-  if (weatherData) {
-    // console.log(weatherData.current);
+  if (currentForecast) {
+    console.log(currentForecast);
+    content = (<div>
+      <img src={currentForecast.day.condition.icon}/>
+    </div>)
   }
   return (
     <div>
       Hello World
 
-      <button onClick={search}>search</button>
+      <button onClick={() => {console.log("ouch")}}>search</button>
+      {content}
     </div>
   )
 }
--- 2020/weather/src/hooks/useWeather.jsx
+++ 2020/weather/src/hooks/useWeather.jsx
@@ -1,5 +1,23 @@
 import { useEffect, useState } from 'react';
+import weather from './../api/weather';
 
 const useWeather = (defaultSearch) => {
-  // const [forcaste, ]
+  const [ forecast, setForecast ] = useState([])
+  
+  useEffect(() => {
+    search(defaultSearch);
+  }, [ defaultSearch ])
+  
+  const search = async (searchInput) => {
+    const response = await weather.get("/forecast.json", {
+      params: {
+        q: searchInput,
+        days: 3
+      }
+    })
+    setForecast(response.data.forecast.forecastday)
+  }
+  return [forecast, search]
 }
+
+export default useWeather;
